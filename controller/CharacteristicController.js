cb2dc417218593cf071c822370bd3f254159b8a0
LAPTOP-I0J3VPD3\German <garodriguezr.11@gmail.com>
2018-08-18 01:57:39 +0200

    First tag to test

--- controller/CharacteristicController.js
+++ controller/CharacteristicController.js
@@ -5,7 +5,8 @@ var br = document.createElement('br');
 characteristicRet.appendChild(br);
 characteristic.valores.forEach(function(value)
 {
-var valor = document.createElement("p");
+var valor = document.createElement("a");
+valor.setAttribute("href","https://amzn.to/2Bt2x1U");
 valor.innerHTML = value.valor + " "+characteristic.unidad;
 characteristicRet.appendChild(valor);
 
